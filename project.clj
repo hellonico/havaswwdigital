8e62ea46502aa11234bdc96db5e249a9af3c9974
Nicolas Modrzyk <hellonico@gmail.com>
2013-01-17 19:51:47 +0900

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject havaswwdigital "1.0"
+(defproject havaswwdigital "1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
